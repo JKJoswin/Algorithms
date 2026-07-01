d0ba83f9d1bf735aaa3fadc78b0291b744584009
Joswin <joswinjayakumar@gmail.com>
2026-07-01 17:43:41 +0530

    Class 1

---
+++ Python/legacy.py
@@ -0,0 +1,5 @@
+import keyword
+
+print(keyword.kwlist)
+print("Hi! I am Joswin from CPS!")
+print("My age is ",13)
---
+++ Python/print.py
@@ -0,0 +1,5 @@
+#program-1
+
+print("Name: Joswin\nAge: 13")
+sum = 10+7
+print("Sum of 10 and 7 is",sum)
